5677f54c415ec419dc2a66dca34d02169da8bc53
jascha <jascha@jaeh.at>
2020-07-16 17:12:37 +0200

    lib/escape: format

--- src/lib/escape.mjs
+++ src/lib/escape.mjs
@@ -1,6 +1,2 @@
 // escape string delimiters in the string
-export const escape = str =>
-  str
-    .replace(/'/g, "\\'")
-    .replace(/`/g, '\\`')
-    .replace(/"/g, '\\"')
+export const escape = str => str.replace(/'/g, "\\'").replace(/`/g, '\\`').replace(/"/g, '\\"')
