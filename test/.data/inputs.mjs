e58706d36a8f53e868cd6d5be621c809d8a21519
jaeh <jascha@jaeh.at>
2021-04-30 00:33:26 +0200

    rewrite tests to expect arrays

--- test/.data/inputs.mjs
+++ test/.data/inputs.mjs
@@ -1,89 +1,89 @@
 export const h1 = {
   md: '# testing',
   html: '<h1 id="testing">testing</h1>',
-  magic: "h1({ id: 'testing' }, 'testing')",
+  magic: ["h1({ id: 'testing' }, 'testing')"],
 }
 
 export const h1Long = {
   md: 'testing\n=========',
   html: '<h1 id="testing">testing</h1>',
-  magic: "h1({ id: 'testing' }, 'testing')",
+  magic: ["h1({ id: 'testing' }, 'testing')"],
 }
 
 export const h2 = {
   md: '## testing',
   html: '<h2 id="testing">testing</h2>',
-  magic: "h2({ id: 'testing' }, 'testing')",
+  magic: ["h2({ id: 'testing' }, 'testing')"],
 }
 export const h3 = {
   md: '### testing',
   html: '<h3 id="testing">testing</h3>',
-  magic: "h3({ id: 'testing' }, 'testing')",
+  magic: ["h3({ id: 'testing' }, 'testing')"],
 }
 export const h4 = {
   md: '#### testing',
   html: '<h4 id="testing">testing</h4>',
-  magic: "h4({ id: 'testing' }, 'testing')",
+  magic: ["h4({ id: 'testing' }, 'testing')"],
 }
 
 export const h5 = {
   md: '##### testing 5',
   html: '<h5 id="testing-5">testing 5</h5>',
-  magic: "h5({ id: 'testing-5' }, 'testing 5')",
+  magic: ["h5({ id: 'testing-5' }, 'testing 5')"],
 }
 
 export const link = {
   md: '[a link](http://example.com)',
   html: '<a href="http://example.com" text="a link"></a>',
-  magic: "Link({ to: 'http://example.com', text: 'a link' })",
+  magic: ["Link({ to: 'http://example.com', text: 'a link' })"],
 }
 
 export const code = {
   md: '<pre><code class="language-bash">code block</pre></code>',
   html: '<pre><code class="language-bash">code block</pre></code>',
-  magic: "pre(code({ class: 'language-bash' }, 'code block'))",
+  magic: ["pre(code({ class: 'language-bash' }, 'code block'))"],
 }
 
 export const codeNoLang = {
   md: '```\ncode block\n```',
   html: '<Pre>code block</Pre>',
-  magic: "Pre('code block')",
+  magic: ["Pre('code block')"],
 }
 
 export const p = {
   md: 'paragraph',
   html: '<p>paragraph</p>',
-  magic: "p('paragraph')",
+  magic: ["p('paragraph')"],
 }
 
 export const sentence = {
   md: "It's very easy to make some words **bold** and other words *italic* with Markdown. You can even [link!](http://example.com)",
   html: "<p>It's very easy to make some words <strong>bold</strong> and other words <em>italic</em> with Markdown. You can even <a href=\"http://example.com\" text=\"link!\"></a>",
-  magic: `p(["It's very easy to make some words ",b('bold'),' and other words ',em('italic'),' with Markdown. You can even ',Link({ to: 'http://example.com', text: 'link!' })])`,
+  magic: [`p(["It's very easy to make some words ",b('bold'),' and other words ',em('italic'),' with Markdown. You can even ',Link({ to: 'http://example.com', text: 'link!' })])`],
 }
 
 export const strong = {
   md: '**bold**',
   html: '<p><b>bold</b></p>',
-  magic: "p(b('bold'))",
+  magic: ["p(b('bold'))"],
 }
 
 export const italic = {
   md: '*italic*',
   html: '<p><em>italic</em></p>',
-  magic: "p(em('italic'))",
+  magic: ["p(em('italic'))"],
 }
 
 export const ul = {
   md: '* listitem 1\n* listitem 2\n  * listitem 2 subitem 1\n  * listitem 2 subitem 2',
   html: '<ul><li>listitem 1</li><li>listitem 2<ul><li>listitem 2 subitem 1</li><li>listitem 2 subitem 2</li></ul></ul>',
-  magic: `ul([li('listitem 1'),li(['listitem 2',ul([li('listitem 2 subitem 1'),li('listitem 2 subitem 2')])])])`
+  magic: [`ul([li('listitem 1'),li(['listitem 2',ul([li('listitem 2 subitem 1'),li('listitem 2 subitem 2')])])])`],
 }
 
 export const ulSingle = {
   md: '* test',
   html: '<ul><li>test</li></ul>',
-  magic: `ul(li('test'))`,
+  magic: [`ul(li('test'))`],
 }
 
 export const img = {
@@ -93,29 +93,29 @@ export const img = {
 export const html = {
   md: '<a href="/testing">text</a>',
   html: '<a href="/testing">text</a>',
-  magic: `Link({ to: '/testing' }, 'text')`,
+  magic: [`Link({ to: '/testing' }, 'text')`],
 }
 
 export const htmlModule = {
   md: '<ModuleName arg="testing">children</ModuleName>',
   html: '<ModuleName arg="testing">children</ModuleName>',
-  magic: `ModuleName({ arg: 'testing' }, 'children')`,
+  magic: [`ModuleName({ arg: 'testing' }, 'children')`],
 }
 
 export const unescapeTest = {
   md: '<div>&lt; &#39; &#8968;</div>',
   html: '<div>&lt; &#39; &#8968;</div>',
-  magic: `div("< ' ⌈")`,
+  magic: [`div("< ' ⌈")`],
 }
 
 export const simpleSvg = {
   input: '<svg><g></g></svg>',
-  expect: 'svg(g())',
+  expect: ['svg(g())'],
 }
 
 export const fillStrokeTestSvg = {
   input: '<svg><g fill-stroke="green"></g></svg>',
-  expect: "svg(g({ fillStroke: 'green' }))",
+  expect: ["svg(g({ fillStroke: 'green' }))"],
 }
 
 export const complexSvg = {
@@ -124,7 +124,7 @@ export const complexSvg = {
 viewBox="0 0 16 16" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1"
 ><g><path d="M32 0 32" /></g>
 `,
-  expect: `
+  expect: [`
 svg({ viewBox: '0 0 16 16', fillRule: 'evenodd', clipRule: 'evenodd', strokeLinejoin: 'round', strokeMiterlimit: '1' }, g(path({ d: 'M32 0 32' })))
-`.trim()
+`.trim()]
 }
